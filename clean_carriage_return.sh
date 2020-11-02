16caac7078348437aedc83b9b8063bc123dd0a94
Avigeant <bobbax4@live.fr>
2020-11-02 12:04:57 +0100

    [ADD] .gitignore and script for removing carriage return in file

---
+++ clean_carriage_return.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+##
+## EPITECH PROJECT, 2020
+## clean_carriage_return
+## File description:
+## Remove carriage return (\r) from your files 
+##
+
+for f in *.c; do sed -i 's/\r$//g' $f; done
